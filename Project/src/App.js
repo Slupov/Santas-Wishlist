defe5dd22547c3890b6fca23bbe13bd50c8ca8a9
Slupov <stoyan.lupov@gmail.com>
2016-12-04 02:29:28 +0200

    Home page transferred to navigation bar (<Header/>)

--- Project/src/App.js
+++ Project/src/App.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/common/Header';
-import Navbar from './components/common/Navbar';
+import {Link} from 'react-router';
 
 class App extends Component {
+
   render() {
+      let message = <p>You are currently not logged in. Please, log in or register to send Santa a letter.</p>;
+      let isLoggedIn = false;
+
+      function drawHeader(isAuthorized) {
+          if (isAuthorized === true) {
+              return <Header>
+                  {sessionStorage.getItem('userType') === "child" &&
+                  <Link to="/letter" className="btn btn-default" activeClassName="btn btn-default active">Send
+                      Letter</Link>
+                  }
+                  <Link to="/about" className="btn btn-default" activeClassName="btn btn-default active">About</Link>
+                  <Link to="/logout" className="btn btn-default"
+                        activeClassName="btn btn-default active">Logout</Link>
+                  <Link to="/presents" className="btn btn-default" activeClassName="btn btn-default active">All
+                      presents</Link>
+                  <Link to="/inbox" className="btn btn-default" activeClassName="btn btn-default active">My Inbox</Link>
+                  {message}
+              </Header>
+          }
+          else {
+              return <Header>
+                  <Link to="/about" className="btn btn-default" activeClassName="btn btn-default active">About</Link>
+                  <Link to="/register" className="btn btn-default"
+                        activeClassName="btn btn-default active">Register</Link>
+                  <Link to="/login" className="btn btn-default" activeClassName="btn btn-default active">Login</Link>
+                  {message}
+              </Header>
+          }
+      }
+
+      //If logged in
+      if (sessionStorage.getItem('username')) {
+          message = <p>Heya, <b>{sessionStorage.getItem('username')}</b>. You can send Santa a mail now :)</p>
+          isLoggedIn = true;
+      }
+
     return (
       <div className="App">
-        <Header>
-            <Navbar/>
-        </Header>
+          {drawHeader(isLoggedIn)}
           {this.props.children}
       </div>
     );
--- Project/src/components/Home/HomePage.js
+++ Project/src/components/Home/HomePage.js
@@ -1,48 +1,12 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router';
 
 export default class HomePage extends Component {
-    render() {
-        let message = <p>You are currently not logged in. Please, log in or register to send Santa a letter.</p>;
-        let isLoggedIn = false;
-
-        function drawContent(isAuthorized) {
-            if (isAuthorized === true) {
-                return <div>
-                    <h1>Home Page</h1>
-                    {sessionStorage.getItem('userType') === "child" &&
-                        <Link to="/letter" className="btn btn-default" activeClassName="btn btn-default active">Send
-                            Letter</Link>
-                    }
-                    <Link to="/about" className="btn btn-default" activeClassName="btn btn-default active">About</Link>
-                    <Link to="/logout" className="btn btn-default"
-                          activeClassName="btn btn-default active">Logout</Link>
-                    <Link to="/presents" className="btn btn-default" activeClassName="btn btn-default active">All
-                        presents</Link>
-                    <Link to="/inbox" className="btn btn-default" activeClassName="btn btn-default active">My Inbox</Link>
-                    {message}
-                </div>
-            }
-            else {
-                return <div>
-                    <h1>Home Page</h1>
-                    <Link to="/about" className="btn btn-default" activeClassName="btn btn-default active">About</Link>
-                    <Link to="/register" className="btn btn-default"
-                          activeClassName="btn btn-default active">Register</Link>
-                    <Link to="/login" className="btn btn-default" activeClassName="btn btn-default active">Login</Link>
-                    {message}
-                </div>
-            }
-        }
-
-        //If logged in
-        if (sessionStorage.getItem('username')) {
-            message = <p>Heya, <b>{sessionStorage.getItem('username')}</b>. You can send Santa a mail now :)</p>
-            isLoggedIn = true;
-        }
-
+    render(){
         return (
-            drawContent(isLoggedIn)
+            <div className="HomePage">
+                <h1>Home page</h1>
+                {this.props.children}
+            </div>
         );
     }
 }
