619feea87df8c7fbbabd0caf4b15c6a049ca5e2d
Maria Velikova <maria.y.velikova@gmail.com>
2016-12-04 02:35:34 +0200

    counter

---
+++ Project/src/components/Home/counter.js
@@ -0,0 +1,10 @@
+function XmasCounter() {
+     let today = new Date();
+    let xmas = new Date(today.getFullYear(), 11, 25);
+    if (today.getMonth() == 11 && today.getDate() > 25) {
+        xmas.setFullYear(xmas.getFullYear() + 1);
+    }
+    let one_day = 1000 * 60 * 60 * 24;
+  return  Math.ceil((xmas.getTime() - today.getTime()) / (one_day)) ;
+}
+export {XmasCounter};
