1398b81b96ec6d33ea06ca96ac2284f31bffb532
Maria Velikova <maria.y.velikova@gmail.com>
2016-12-04 02:47:41 +0200

    .

--- Project/src/components/Home/HomePage.js
+++ Project/src/components/Home/HomePage.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-
+import {XmasCounter} from './counter';
 export default class HomePage extends Component {
     render(){
         return (
             <div className="HomePage">
-                <h1>Home page</h1>
+                <h1>{XmasCounter()}days left</h1>
                 {this.props.children}
             </div>
         );
